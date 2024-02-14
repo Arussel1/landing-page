9191f080042da1b13468e2e2ace7c71aaec70a68
Arussel <anhminh05102004@gmail.com>
2024-02-13 21:53:10 -0500

    Add event listener to buttons

---
+++ script.js
@@ -0,0 +1,6 @@
+let buttons = document.querySelectorAll("button");
+for(let button of buttons){
+    button.addEventListener("click", () =>{
+        location.href = "https://arussel1.github.io./signUpForm";
+    });
+}
